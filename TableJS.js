dc7b6f35500a5b02505e02b98d341da574383ebb
arightruckuss <66334071+arightruckuss@users.noreply.github.com>
2020-09-07 21:48:01 +0100

    Add files via upload

---
+++ TableJS.js
@@ -0,0 +1,293 @@
+//Functions to add and provide the sum of table fields
+function totalR1() {
+    var td11 = Number(document.getElementById("td11").value);
+    var td21 = Number(document.getElementById("td21").value);
+    var td31 = Number(document.getElementById("td31").value); 
+    var td41 = Number(document.getElementById("td41").value); 
+    var td51 = Number(document.getElementById("td51").value); 
+    var td61 = Number(document.getElementById("td61").value); 
+    
+    var tdR1 = td11 + td21 + td31 + td41 + td51 + td61;
+    
+    document.getElementById("tdR1").value = tdR1;
+}
+
+function totalR2() {
+    var td12 = Number(document.getElementById("td12").value);
+    var td22 = Number(document.getElementById("td22").value);
+    var td32 = Number(document.getElementById("td32").value); 
+    var td42 = Number(document.getElementById("td42").value); 
+    var td52 = Number(document.getElementById("td52").value); 
+    var td62 = Number(document.getElementById("td62").value); 
+    var tdR2 = td12 + td22 + td32 + td42 + td52 + td62;
+    
+    document.getElementById("tdR2").value = tdR2;
+}
+
+function totalR3() {
+    var td13 = Number(document.getElementById("td13").value);
+    var td23 = Number(document.getElementById("td23").value);
+    var td33 = Number(document.getElementById("td33").value); 
+    var td43 = Number(document.getElementById("td43").value); 
+    var td53 = Number(document.getElementById("td53").value); 
+    var td63 = Number(document.getElementById("td63").value); 
+    var tdR3 = td13 + td23 + td33 + td43 + td53 + td63;
+    
+    document.getElementById("tdR3").value = tdR3;
+}
+
+function totalR4() {
+    var td14 = Number(document.getElementById("td14").value);
+    var td24 = Number(document.getElementById("td24").value);
+    var td34 = Number(document.getElementById("td34").value); 
+    var td44 = Number(document.getElementById("td44").value); 
+    var td54 = Number(document.getElementById("td54").value); 
+    var td64 = Number(document.getElementById("td64").value); 
+    var tdR4 = td14 + td24 + td34 + td44 + td54 + td64;
+    
+    document.getElementById("tdR4").value = tdR4;
+}
+
+function totalR5() {
+    var td15 = Number(document.getElementById("td15").value);
+    var td25 = Number(document.getElementById("td25").value);
+    var td35 = Number(document.getElementById("td35").value); 
+    var td45 = Number(document.getElementById("td45").value); 
+    var td55 = Number(document.getElementById("td55").value); 
+    var td65 = Number(document.getElementById("td65").value); 
+    var tdR5 = td15 + td25 + td35 + td45 + td55 + td65;
+    
+    document.getElementById("tdR5").value = tdR5;
+}
+
+function totalR6() {
+    var td16 = Number(document.getElementById("td16").value);
+    var td26 = Number(document.getElementById("td26").value);
+    var td36 = Number(document.getElementById("td36").value); 
+    var td46 = Number(document.getElementById("td46").value); 
+    var td56 = Number(document.getElementById("td56").value); 
+    var td66 = Number(document.getElementById("td66").value); 
+    var tdR6 = td16 + td26 + td36 + td46 + td56 + td66;
+    
+    document.getElementById("tdR6").value = tdR6;
+}
+function totalR7() {
+    var td17 = Number(document.getElementById("td17").value);
+    var td27 = Number(document.getElementById("td27").value);
+    var td37 = Number(document.getElementById("td37").value); 
+    var td47 = Number(document.getElementById("td47").value); 
+    var td57 = Number(document.getElementById("td57").value); 
+    var td67 = Number(document.getElementById("td67").value); 
+    var tdR7 = td17 + td27 + td37 + td47 + td57 + td67;
+    
+    document.getElementById("tdR7").value = tdR7;
+}
+
+function totalR8() {
+    var td18 = Number(document.getElementById("td18").value);
+    var td28 = Number(document.getElementById("td28").value);
+    var td38 = Number(document.getElementById("td38").value); 
+    var td48 = Number(document.getElementById("td48").value); 
+    var td58 = Number(document.getElementById("td58").value); 
+    var td68 = Number(document.getElementById("td68").value); 
+    var tdR8 = td18 + td28 + td38 + td48 + td58 + td68;
+    
+    document.getElementById("tdR8").value = tdR8;
+}
+
+function totalR9() {
+    var td19 = Number(document.getElementById("td19").value);
+    var td29 = Number(document.getElementById("td29").value);
+    var td39 = Number(document.getElementById("td39").value); 
+    var td49 = Number(document.getElementById("td49").value); 
+    var td59 = Number(document.getElementById("td59").value); 
+    var td69 = Number(document.getElementById("td69").value); 
+    var tdR9 = td19 + td29 + td39 + td49 + td59 + td69;
+    
+    document.getElementById("tdR9").value = tdR9;
+}
+
+function totalR10() {
+    var td110 = Number(document.getElementById("td110").value);
+    var td210 = Number(document.getElementById("td210").value);
+    var td310 = Number(document.getElementById("td310").value); 
+    var td410 = Number(document.getElementById("td410").value); 
+    var td510 = Number(document.getElementById("td510").value); 
+    var td610 = Number(document.getElementById("td610").value); 
+    var tdR10 = td110 + td210 + td310 + td410 + td510 + td610;
+    
+    document.getElementById("tdR10").value = tdR10;
+}
+
+function totalR11() {
+    var td111 = Number(document.getElementById("td111").value);
+    var td211 = Number(document.getElementById("td211").value);
+    var td311 = Number(document.getElementById("td311").value); 
+    var td411 = Number(document.getElementById("td411").value); 
+    var td511 = Number(document.getElementById("td511").value); 
+    var td611 = Number(document.getElementById("td611").value); 
+    var tdR11 = td111 + td211 + td311 + td411 + td511 + td611;
+    
+    document.getElementById("tdR11").value = tdR11;
+}
+
+function totalR12() {
+    var td112 = Number(document.getElementById("td112").value);
+    var td212 = Number(document.getElementById("td212").value);
+    var td312 = Number(document.getElementById("td312").value); 
+    var td412 = Number(document.getElementById("td412").value); 
+    var td512 = Number(document.getElementById("td512").value); 
+    var td612 = Number(document.getElementById("td612").value); 
+    var tdR12 = td112 + td212 + td312 + td412 + td512 + td612;
+    
+    document.getElementById("tdR12").value = tdR12;
+}
+
+function totalR13() {
+    var tdR1 = Number(document.getElementById("tdR1").value);
+    var tdR2 = Number(document.getElementById("tdR2").value);
+    var tdR3 = Number(document.getElementById("tdR3").value); 
+    var tdR4 = Number(document.getElementById("tdR4").value); 
+    var tdR5 = Number(document.getElementById("tdR5").value); 
+    var tdR6 = Number(document.getElementById("tdR6").value);
+    var tdR7 = Number(document.getElementById("tdR7").value);
+    var tdR8 = Number(document.getElementById("tdR8").value);   
+    var tdR9 = Number(document.getElementById("tdR9").value);
+    var tdR10 = Number(document.getElementById("tdR10").value);
+    var tdR11 = Number(document.getElementById("tdR11").value);
+    var tdR12 = Number(document.getElementById("tdR12").value);
+    var tdR13 = tdR1 + tdR2 + tdR3 + tdR4 + tdR5 + tdR6 + tdR7 + tdR8 + tdR9 + tdR10 + tdR11 + tdR12;
+    
+}
+
+function totalR13() {
+    var tdR1 = Number(document.getElementById("tdR1").value);
+    var tdR2 = Number(document.getElementById("tdR2").value);
+    var tdR3 = Number(document.getElementById("tdR3").value); 
+    var tdR4 = Number(document.getElementById("tdR4").value); 
+    var tdR5 = Number(document.getElementById("tdR5").value); 
+    var tdR6 = Number(document.getElementById("tdR6").value);
+    var tdR7 = Number(document.getElementById("tdR7").value);
+    var tdR8 = Number(document.getElementById("tdR8").value);   
+    var tdR9 = Number(document.getElementById("tdR9").value);
+    var tdR10 = Number(document.getElementById("tdR10").value);
+    var tdR11 = Number(document.getElementById("tdR11").value);
+    var tdR12 = Number(document.getElementById("tdR12").value);
+    var tdR13 = tdR1 + tdR2 + tdR3 + tdR4 + tdR5 + tdR6 + tdR7 + tdR8 + tdR9 + tdR10 + tdR11 + tdR12;
+    
+    document.getElementById("tdR13").value = tdR13;
+}
+function C1() {
+    var tdC11 = Number(document.getElementById("td11").value);
+    var tdC12 = Number(document.getElementById("td12").value);
+    var tdC13 = Number(document.getElementById("td13").value);
+    var tdC14 = Number(document.getElementById("td14").value);
+    var tdC15 = Number(document.getElementById("td15").value);
+    var tdC16 = Number(document.getElementById("td16").value);
+    var tdC17 = Number(document.getElementById("td17").value);
+    var tdC18 = Number(document.getElementById("td18").value);
+    var tdC19 = Number(document.getElementById("td19").value);
+    var tdC110 = Number(document.getElementById("td110").value);
+    var tdC111 = Number(document.getElementById("td111").value);
+    var tdC112 = Number(document.getElementById("td112").value);    
+        
+    var C1 = tdC11 + tdC12 + tdC13 + tdC14 + tdC15 + tdC16 + tdC17 + tdC18 + tdC19 + tdC110 + tdC111 + tdC112;
+    
+    document.getElementById("td113").value = C1;
+    }
+    
+function C2() {
+    var tdC21 = Number(document.getElementById("td21").value);
+    var tdC22 = Number(document.getElementById("td22").value)   
+    var tdC23 = Number(document.getElementById("td23").value);
+    var tdC24 = Number(document.getElementById("td24").value);
+    var tdC25 = Number(document.getElementById("td25").value);
+    var tdC26 = Number(document.getElementById("td26").value);
+    var tdC27 = Number(document.getElementById("td27").value);
+    var tdC28 = Number(document.getElementById("td28").value); var tdC29 = Number(document.getElementById("td29").value);
+    var tdC210 = Number(document.getElementById("td210").value);
+    var tdC211 = Number(document.getElementById("td211").value);
+    var tdC212 = Number(document.getElementById("td212").value);    
+    
+    var C2 = tdC21 + tdC22 + tdC23 + tdC24 + tdC25 + tdC26 + tdC27 + tdC28 + tdC29 + tdC210 + tdC211 + tdC212;
+    
+    document.getElementById("td213").value = C2;
+    }
+
+function C3() {
+    var tdC31 = Number(document.getElementById("td31").value);
+    var tdC32 = Number(document.getElementById("td32").value)   
+    var tdC33 = Number(document.getElementById("td33").value);
+    var tdC34 = Number(document.getElementById("td34").value);
+    var tdC35 = Number(document.getElementById("td35").value);
+    var tdC36 = Number(document.getElementById("td36").value);
+    var tdC37 = Number(document.getElementById("td37").value);
+    var tdC38 = Number(document.getElementById("td38").value); var tdC39 = Number(document.getElementById("td39").value);
+    var tdC310 = Number(document.getElementById("td310").value);
+    var tdC311 = Number(document.getElementById("td311").value);
+    var tdC312 = Number(document.getElementById("td312").value);    
+    
+    var C3 = tdC31 + tdC32 + tdC33 + tdC34 + tdC35 + tdC36 + tdC37 + tdC38 + tdC39 + tdC310 + tdC311 + tdC312;
+    
+    document.getElementById("td313").value = C3;
+    }
+
+function C4() {
+    var tdC41 = Number(document.getElementById("td41").value);
+    var tdC42 = Number(document.getElementById("td42").value); 
+    var tdC43 = Number(document.getElementById("td43").value);
+    var tdC44 = Number(document.getElementById("td44").value);
+    var tdC45 = Number(document.getElementById("td45").value);
+    var tdC46 = Number(document.getElementById("td46").value);
+    var tdC47 = Number(document.getElementById("td47").value);
+    var tdC48 = Number(document.getElementById("td48").value); 
+    var tdC49 = Number(document.getElementById("td49").value);
+    var tdC410 = Number(document.getElementById("td410").value);
+    var tdC411 = Number(document.getElementById("td411").value);
+    var tdC412 = Number(document.getElementById("td412").value);
+
+    
+    var C4 = tdC41 + tdC42 + tdC43 + tdC44 + tdC45 + tdC46 + tdC47 + tdC48 + tdC49 + tdC410 + tdC411 + tdC412;
+        
+    document.getElementById("td413").value = C4;
+    }
+
+function C5() {
+    var tdC51 = Number(document.getElementById("td51").value);
+    var tdC52 = Number(document.getElementById("td52").value); 
+    var tdC53 = Number(document.getElementById("td53").value);
+    var tdC54 = Number(document.getElementById("td54").value);
+    var tdC55 = Number(document.getElementById("td55").value);
+    var tdC56 = Number(document.getElementById("td56").value);
+    var tdC57 = Number(document.getElementById("td57").value);
+    var tdC58 = Number(document.getElementById("td58").value); 
+    var tdC59 = Number(document.getElementById("td59").value);
+    var tdC510 = Number(document.getElementById("td510").value);
+    var tdC511 = Number(document.getElementById("td511").value);
+    var tdC512 = Number(document.getElementById("td512").value);
+
+    var C5 = tdC51 + tdC52 + tdC53 + tdC54 + tdC55 + tdC56 + tdC57 + tdC58 + tdC59 + tdC510 + tdC511 + tdC512;
+        
+    document.getElementById("td513").value = C5;
+    }
+
+function C6() {
+    var tdC61 = Number(document.getElementById("td61").value);
+    var tdC62 = Number(document.getElementById("td62").value); 
+    var tdC63 = Number(document.getElementById("td63").value);
+    var tdC64 = Number(document.getElementById("td64").value);
+    var tdC65 = Number(document.getElementById("td65").value);
+    var tdC66 = Number(document.getElementById("td66").value);
+    var tdC67 = Number(document.getElementById("td67").value);
+    var tdC68 = Number(document.getElementById("td68").value); 
+    var tdC69 = Number(document.getElementById("td69").value);
+    var tdC610 = Number(document.getElementById("td610").value);
+    var tdC611 = Number(document.getElementById("td611").value);
+    var tdC612 = Number(document.getElementById("td612").value);
+    
+    var C6 = tdC61 + tdC62 + tdC63 + tdC64 + tdC65 + tdC66 + tdC67 + tdC68 + tdC69 + tdC610 + tdC611 + tdC612;
+        
+    document.getElementById("td613").value = C6;
+    }
+
+    
